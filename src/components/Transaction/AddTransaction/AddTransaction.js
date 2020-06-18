152ee7a1558c2fd14696f64c1f96611f81a42448
AbdulrehmanTalat <66178386+AbdulrehmanTalat@users.noreply.github.com>
2020-06-18 23:33:18 +0500

    Changes in transaction

--- src/components/Transaction/AddTransaction/AddTransaction.js
+++ src/components/Transaction/AddTransaction/AddTransaction.js
@@ -13,6 +13,9 @@ export const AddTransaction = () => {
         if(amount === 0){
          alert("Amount Should not be 0")
          }
+         if(amount < 0){
+            alert("Amount should not be in -ve")
+        }
          else if(amount !== 0 && text !== null){
         const newTransaction = {
             id: Math.floor(Math.random() * 100000000),
@@ -22,10 +25,30 @@ export const AddTransaction = () => {
         addTransaction(newTransaction);
     }
     }
+    const addexpense = e => {
+        e.preventDefault();
+        if(text === ''){
+            alert("Please enter a valid Title")
+           } 
+        if(amount === 0){
+         alert("Amount Should not be 0")
+         }
+         if(amount < 0){
+             alert("Amount should not be in -ve")
+         }
+         else if(amount !== 0 && text !== null){
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount: -amount
+        }
+        addTransaction(newTransaction);
+    }
+    }
     return (
         <div>
             <h3>Add New Transaction</h3>
-            <form onSubmit={onSubmit}>
+            <form >
             <div className="form-control">
             <label htmlFor="text"></label>
             <input type="text" value={text} onChange={(e)=> setText(e.target.value)} placeholder="Enter Type of Transaction" />
@@ -34,7 +57,11 @@ export const AddTransaction = () => {
           
             <input type="number" min="0" value={amount} onChange={(e)=> setAmount(e.target.value)}  placeholder="Enter Amount"/>
             </div>
-            <button className="btn">Add Transaction</button>
+            <div className="row">
+            <button className="btn-inc" onClick={onSubmit}>Add Income</button>  
+            <button className="btn-exp" onClick={addexpense}>Add Expense</button>
+          
+            </div>
             </form>
         </div>
     )
